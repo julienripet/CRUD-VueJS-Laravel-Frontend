6e90f1e8e83352cde6cd98d8198f68805086e9bf
julien ripet <ripetjulien@gmail.com>
2021-09-16 20:39:23 +0200

    Seting up theme and fonts

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  >
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <p>test</p>
+    <Button label="test"> </Button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import {createApp} from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
+
+// Styles, themes, icons
+import "./assets/themes/default.css"                //theme
+import "primevue/resources/primevue.min.css"        //core css
+import "primeicons/primeicons.css"                  //icons
+
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Button from 'primevue/button'
+
 const app = createApp(App);
 
 app.use(PrimeVue);
 
+//Globaly usable Components
+app.component("DataTable", DataTable);
+app.component("Column", Column);
+app.component("Button", Button);
+
+app.mount('#app')
